b094cc9d8ceaea756543ec80286bce836af5305d
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-14 00:22:14 +0200

    feat(main): Registered listeners.

--- src/fr/hashtek/spigot/breakffa/BreakFFA.java
+++ src/fr/hashtek/spigot/breakffa/BreakFFA.java
@@ -151,6 +151,10 @@ public class BreakFFA extends JavaPlugin implements HashLoggable {
         this.pluginManager.registerEvents(new ListenerMove(this), this);
         this.pluginManager.registerEvents(new ListenerBreak(this), this);
         this.pluginManager.registerEvents(new ListenerPlace(this), this);
+        this.pluginManager.registerEvents(new ListenerDrop(this), this);
+        this.pluginManager.registerEvents(new ListenerDamageEvent(this), this);
+        this.pluginManager.registerEvents(new ListenerDeath(this), this);
+        this.pluginManager.registerEvents(new ListenerRespawn(this), this);
 
         this.logger.info(this, "Listeners loaded!");
     }
