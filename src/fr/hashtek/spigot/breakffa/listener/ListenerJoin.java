c9c0136ceef5bccdea273fae50db833688615b07
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-13 11:10:03 +0200

    feat(listener): Join procedure started.

--- src/fr/hashtek/spigot/breakffa/listener/ListenerJoin.java
+++ src/fr/hashtek/spigot/breakffa/listener/ListenerJoin.java
@@ -3,6 +3,8 @@ package fr.hashtek.spigot.breakffa.listener;
 import fr.hashtek.hashlogger.HashLoggable;
 import fr.hashtek.spigot.breakffa.BreakFFA;
 import fr.hashtek.spigot.breakffa.game.GameManager;
+import fr.hashtek.spigot.breakffa.player.PlayerData;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,12 +25,16 @@ public class ListenerJoin implements Listener, HashLoggable
     @EventHandler
     public void onJoin(PlayerJoinEvent event)
     {
-        Player player = event.getPlayer();
-        GameManager gameManager = this.main.getGameManager();
+        final Player player = event.getPlayer();
+        final GameManager gameManager = this.main.getGameManager();
+        final PlayerData playerData = new PlayerData(player, gameManager);
+
+        gameManager.addPlayerData(player, playerData);
 
         event.setJoinMessage(null);
 
-        player.teleport(gameManager.getLobbySpawnLocation());
+        player.setGameMode(GameMode.SURVIVAL);
+        playerData.getPlayerManager().backToLobby();
     }
 
 }
