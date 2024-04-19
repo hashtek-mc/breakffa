72206ffb84143a8100e71656e51762b1f33de139
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-19 17:50:48 +0200

    docs(player/settings): Added Javadoc.

--- src/fr/hashtek/spigot/breakffa/player/PlayerSettings.java
+++ src/fr/hashtek/spigot/breakffa/player/PlayerSettings.java
@@ -5,25 +5,40 @@ public class PlayerSettings
 
     public static final String DEFAULT_HOTBAR_LAYOUT = "sptgb    ";
 
+
     private String hotbarLayout;
 
 
+    /**
+     * Creates a new instance of PlayerSettings.
+     */
     public PlayerSettings()
     {
         this(DEFAULT_HOTBAR_LAYOUT);
     }
 
+    /**
+     * Creates a new instance of PlayerSettings (with custom hotbar layout).
+     *
+     * @param   hotbarLayout    Player's Hotbar layout
+     */
     public PlayerSettings(String hotbarLayout)
     {
         this.hotbarLayout = hotbarLayout;
     }
 
 
+    /**
+     * @return  Hotbar layout
+     */
     public String getHotbarLayout()
     {
         return this.hotbarLayout;
     }
 
+    /**
+     * @param   hotbarLayout    New Hotbar layout
+     */
     public void setHotbarLayout(String hotbarLayout)
     {
         this.hotbarLayout = hotbarLayout;
