ea6fc8776d34dac7802c37512e636a94f8d8760a
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-04-23 13:22:08 +0200

    feat(death): Added death reasons.

---
+++ src/main/java/fr/hashtek/spigot/breakffa/death/DeathReason.java
@@ -0,0 +1,64 @@
+package fr.hashtek.spigot.breakffa.death;
+
+import org.bukkit.ChatColor;
+
+public enum DeathReason
+{
+
+    VOID        (ChatColor.WHITE        + "࿓",  "est tombé",        "à cause de"    ),
+    EXPLODE     (ChatColor.RED          + "✴",  "a explosé",        "à cause de"    ),
+    INTOXICATED (ChatColor.DARK_GREEN   + "☣",  "a été intoxiqué",  "par"           ),
+    MELEE       (ChatColor.DARK_RED     + "⚔",  "a été poignardé",  "par"           ),
+    FIRE        (ChatColor.GOLD         + "♨",  "a fondu",          "à cause de"    ),
+    SHOOT       (ChatColor.YELLOW       + "➵",  "a été fusillé",    "par"           ),
+    DROWN       (ChatColor.BLUE         + "〰",  "s'est noyé",       "à cause de"    ),
+    SUFFOCATION (ChatColor.DARK_AQUA    + "♻",  "a suffoqué",       "à cause de"    ),
+    SQUASHED    (ChatColor.BLACK        + "⏍",  "a été écrasé",     "par"           ),
+    OTHER       (ChatColor.DARK_GRAY    + "✖",  "est mort",         "à cause de"    );
+
+
+    private final String symbol;
+    private final String reason;
+    private final String by;
+
+
+    /**
+     * Creates a new Death reason.
+     *
+     * @param   symbol  Death symbol
+     * @param   reason  Death reason
+     * @param   by      Death author
+     */
+    DeathReason(String symbol, String reason, String by)
+    {
+        this.symbol = symbol;
+        this.reason = reason;
+        this.by = by;
+    }
+
+
+    /**
+     * @return  Death symbol
+     */
+    public String getSymbol()
+    {
+        return this.symbol;
+    }
+
+    /**
+     * @return  Death reason
+     */
+    public String getReason()
+    {
+        return this.reason;
+    }
+
+    /**
+     * @return  Death author
+     */
+    public String getBy()
+    {
+        return this.by;
+    }
+
+}
