b87e8bf2934f3fbdc31ccf1c2a21a0c869ac9864
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-09-26 21:53:10 +0200

    fix(listener>chat): Fixed wrong format due to API migration mistake.

--- src/main/java/fr/hashtek/spigot/breakffa/listener/ListenerChat.java
+++ src/main/java/fr/hashtek/spigot/breakffa/listener/ListenerChat.java
@@ -4,13 +4,16 @@ import fr.hashtek.spigot.breakffa.BreakFFA;
 import fr.hashtek.tekore.bukkit.Tekore;
 import fr.hashtek.tekore.common.Rank;
 import fr.hashtek.tekore.common.player.PlayerData;
+import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
 
 public class ListenerChat implements Listener
 {
@@ -39,11 +42,23 @@ public class ListenerChat implements Listener
         final PlayerData playerData = this.core.getPlayerManager(player).getData();
         final Rank playerRank = playerData.getRank();
 
-        event.message(Component.text(
-            playerRank.getColor() + player.getName() + "@" + playerRank.getName()
-            + ChatColor.DARK_GRAY + " $ "
-            + ChatColor.WHITE + ((TextComponent) event.message()).content()
-        ));
+        event.renderer(new ChatRenderer() {
+            @NotNull
+            @Override
+            public Component render(
+                @NotNull Player source,
+                @NotNull Component sourceDisplayName,
+                @NotNull Component message,
+                @NotNull Audience viewer
+            ) {
+                return Component.text(
+                    playerRank.getColor() + player.getName() + "@" + playerRank.getName()
+                        + ChatColor.DARK_GRAY + " $ "
+                        + ChatColor.WHITE + ((TextComponent) message).content()
+                );
+            }
+        });
+
     }
 
 }
