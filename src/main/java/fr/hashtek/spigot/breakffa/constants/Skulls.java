184ce335e113a2123b244a9c80b9c839024439f1
Lysandre BOURSETTE <lysandre.boursette@epitech.eu>
2024-10-22 18:04:34 +0200

    feat(constants): Added a Skulls class that will store the base64 value of every Skull used in the game.

---
+++ src/main/java/fr/hashtek/spigot/breakffa/constants/Skulls.java
@@ -0,0 +1,12 @@
+package fr.hashtek.spigot.breakffa.constants;
+
+public class Skulls
+{
+
+    public static final String COSMETIC_BLOCK_CATEGORY =
+        "&6eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjY0YTQxNTk0YTEyYzlmY2Q4Y2I3M2VkMzg0NDlhYTY4MGQ5ZDcyMDg3ZWQzYTE1M2ZiNjViZjI2N2RjOTljYiJ9fX0=";
+
+    public static final String COSMETIC_HATS_CATEGORY =
+        "&6eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjA5NWRlZWZiMzQ5NWQ5ZDdmYzhiM2NiMzkwYTc3M2ViMzZlOGRjMjkxMmEyZGU5YTViYTJmOTlkNWM2YmEyNCJ9fX0=";
+
+}
